d284d8fb20c25462c1037efd1a52c4df9bd0240e
Jacob Fenton <jsfenfen@gmail.com>
2017-02-06 12:31:57 -0800

    update to django 1.10

--- house_disburse/house_disburse/wsgi.py
+++ house_disburse/house_disburse/wsgi.py
@@ -1,14 +1,16 @@
 """
-WSGI config for house_disburse project.
+WSGI config for blue project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "house_disburse.settings")
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blue.settings")
+
 application = get_wsgi_application()
--- house_disburse/sql_scripts/insert_quarters.sql
+++ house_disburse/sql_scripts/insert_quarters.sql
@@ -1,58 +1,88 @@
-copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/house_disbursements/house_disburse/disbursements/data/2014Q4-detail.csv' with CSV header;
+-- 2016: 
+
+-- copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2016Q4-detail.csv' with CSV header;
 
 
 
-copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/house_disbursements/house_disburse/disbursements/data/2014Q3-detail.csv' with CSV header;
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2016Q3-detail.csv' with CSV header;
 
-copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/house_disbursements/house_disburse/disbursements/data/2014Q2-detail.csv' with CSV header;
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2016Q2-detail.csv' with CSV header;
 
 
-copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/house_disbursements/house_disburse/disbursements/data/2014Q1-detail.csv' with CSV header;
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2016Q1-detail.csv' with CSV header;
 
----
 
-copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/house_disbursements/house_disburse/disbursements/data/2013Q4-detail.csv' with CSV header;
+-- 2015: 
 
-copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/house_disbursements/house_disburse/disbursements/data/2013Q3-detail.csv' with CSV header;
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2015Q4-detail.csv' with CSV header;
 
-copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/house_disbursements/house_disburse/disbursements/data/2013Q2-detail.csv' with CSV header;
 
-copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/house_disbursements/house_disburse/disbursements/data/2013Q1-detail.csv' with CSV header;
+
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2015Q3-detail.csv' with CSV header;
+
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2015Q2-detail.csv' with CSV header;
+
+
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2015Q1-detail.csv' with CSV header;
+
+
+-- 2014: 
+
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2014Q4-detail.csv' with CSV header;
+
+
+
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2014Q3-detail.csv' with CSV header;
+
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2014Q2-detail.csv' with CSV header;
+
+
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2014Q1-detail.csv' with CSV header;
+
+--- 2013 
+
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2013Q4-detail.csv' with CSV header;
+
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2013Q3-detail.csv' with CSV header;
+
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2013Q2-detail.csv' with CSV header;
+
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2013Q1-detail.csv' with CSV header;
 
 -- 2012 
 
-copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/house_disbursements/house_disburse/disbursements/data/2012Q4-detail.csv' with CSV header;
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2012Q4-detail.csv' with CSV header;
 
-copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/house_disbursements/house_disburse/disbursements/data/2012Q3-detail.csv' with CSV header;
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2012Q3-detail.csv' with CSV header;
 
-copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/house_disbursements/house_disburse/disbursements/data/2012Q2-detail.csv' with CSV header;
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2012Q2-detail.csv' with CSV header;
 
-copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/house_disbursements/house_disburse/disbursements/data/2012Q1-detail.csv' with CSV header;
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2012Q1-detail.csv' with CSV header;
 
 -- 2011
 
-copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/house_disbursements/house_disburse/disbursements/data/2011Q4-detail.csv' with CSV header;
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2011Q4-detail.csv' with CSV header;
 
-copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/house_disbursements/house_disburse/disbursements/data/2011Q3-detail.csv' with CSV header;
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2011Q3-detail.csv' with CSV header;
 
-copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/house_disbursements/house_disburse/disbursements/data/2011Q2-detail.csv' with CSV header;
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2011Q2-detail.csv' with CSV header;
 
-copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/house_disbursements/house_disburse/disbursements/data/2011Q1-detail.csv' with CSV header;
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2011Q1-detail.csv' with CSV header;
 
 -- 2010
 
-copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/house_disbursements/house_disburse/disbursements/data/2010Q4-detail.csv' with CSV header;
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2010Q4-detail.csv' with CSV header;
 
 
-copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/house_disbursements/house_disburse/disbursements/data/2010Q3-detail.csv' with CSV header;
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2010Q3-detail.csv' with CSV header;
 
-copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/house_disbursements/house_disburse/disbursements/data/2010Q2-detail.csv' with CSV header;
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2010Q2-detail.csv' with CSV header;
 
-copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/house_disbursements/house_disburse/disbursements/data/2010Q1-detail.csv' with CSV header;
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2010Q1-detail.csv' with CSV header;
 
 -- 2009
 
-copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/house_disbursements/house_disburse/disbursements/data/2009Q4-detail.csv' with CSV header;
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2009Q4-detail.csv' with CSV header;
 
 
-copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/house_disbursements/house_disburse/disbursements/data/2009Q3-detail.csv' with CSV header;
+copy disbursements_expenditure(bioguide_id, office, quarter, category, date_raw, payee, start_date_raw, end_date_raw, purpose, amount_raw, year_raw, transcode, transcodelong, recordid, recip_orig) from '/Users/jfenton/github-whitelabel/disbursements/house_disbursements/house_disburse/disbursements/data/2009Q3-detail.csv' with CSV header;
